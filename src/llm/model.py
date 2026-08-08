f00bf378e89707654d23011b815965a7adacf796
Kevin Arnulfo Castro Lozano <kcastro-@car8s4.42barcelona.com>
2026-08-09 00:04:01 +0200

    Done and understood tensors, batch and did the encode method cleaner

--- src/llm/model.py
+++ src/llm/model.py
@@ -11,7 +11,27 @@ class LLM:
     de inferencia necesarias para el resto del sistema.
     """
     def __init__(self, ):
+        """
+
+        """
         try:
             self._model = Small_LLM_Model()
         except Exception as e:
             raise LLMError(f"Error al instanciar el LLM: {e}")
+
+    def encode(self, text: str) -> list[int]:
+        """
+        Tokeniza  un texto y devuelve sus token IDs.
+
+        Args:
+            text: Texto que se desea tokenizar.
+        Returns:
+            Lista de enteros donde cada entero representa un token ID.
+        Raises:
+            LLMError: Si ocurre un error durante la tokenizacion.
+        """
+        try:
+            input_ids = self._model.encode(text)
+            return input_ids.squeeze(0).tolist()
+        except Exception as e:
+            raise LLMError(f"Error encoding text: {e}") from e
